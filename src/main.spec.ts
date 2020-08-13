7080bb87bd2803388149f52e268998688440a323
Christos Hrousis <christos.hrousis@gmail.com>
2020-08-13 13:51:58 +1000

    :white_check_mark: Write out pricing rules

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,6 +1,58 @@
 import { Checkout, pricingRules } from "./main";
 
-describe("Example Scenarios", () => {
+describe("Expected Behaviours", () => {
+  test("Scanning a product should populate a collection of those products.", () => {
+    const checkout = new Checkout(pricingRules);
+    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
+    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
+    expect(checkout.scannedSKUs.length).toBe(2);
+  });
+
+  test("Total should return a formatted final price string.", () => {
+    const checkout = new Checkout(pricingRules);
+    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
+    expect(checkout.total()).toBe("$549.99");
+  });
+
+  test("Total should return a formatted final price string.", () => {
+    const checkout = new Checkout(pricingRules);
+    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
+    expect(checkout.total()).toBe("$549.99");
+  });
+});
+
+describe("Pricing Rules from Spec", () => {
+  test("we're going to have a 3 for 2 deal on Apple TVs. For example, if you buy 3 Apple TVs, you will pay the price of 2 only", () => {
+    const discount = pricingRules.threeForTwo.rule([
+      { sku: "atv", name: "Apple TV", price: 109.5 },
+      { sku: "atv", name: "Apple TV", price: 109.5 },
+      { sku: "atv", name: "Apple TV", price: 109.5 }
+    ]);
+    expect(discount).toBe(109.5);
+  });
+
+  test("the brand new Super iPad will have a bulk discounted applied, where the price will drop to $499.99 each, if someone buys more than 4", () => {
+    const discount = pricingRules.bulkDiscount.rule([
+      { sku: "ipd", name: "Super iPad", price: 549.99 },
+      { sku: "ipd", name: "Super iPad", price: 549.99 },
+      { sku: "ipd", name: "Super iPad", price: 549.99 },
+      { sku: "ipd", name: "Super iPad", price: 549.99 },
+      { sku: "ipd", name: "Super iPad", price: 549.99 }
+    ])
+    expect(discount).toBe(5 * (549.99 - 499.99));
+  });
+
+  test("we will bundle in a free VGA adapter free of charge with every MacBook Pro sold", () => {
+    const discount = pricingRules.adapterBundle.rule([
+      { sku: "mbp", name: "MacBook Pro", price: 1399.99 },
+    ])
+    // Discount is the price of a VGA adapter.
+    expect(discount).toBe(30.0);
+  });
+});
+
+
+describe("Provided Example Scenarios", () => {
   test("When SKUs Scanned: atv, atv, atv, vga Total expected: $249.00", () => {
     expect(false === false).toBe(false);
   });
@@ -9,14 +61,5 @@ describe("Example Scenarios", () => {
   });
   test("SKUs Scanned: mbp, vga, ipd Total expected: $1949.98", () => {
     expect(false === false).toBe(false);
-  });
-});
-
-describe("Expected Behaviours", () => {
-  test("Scanning a product should populate a collection of those products.", () => {
-    const checkout = new Checkout(pricingRules);
-    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
-    checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 });
-    expect(checkout.scannedSKUs.length).toBe(2);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,30 @@ export const products: Product[] = [
  * the brand new Super iPad will have a bulk discounted applied, where the price will drop to $499.99 each, if someone buys more than 4
  * we will bundle in a free VGA adapter free of charge with every MacBook Pro sold
  */
-
 export const pricingRules = {
-  threeForTwo: { rule: () => {}, products: ["atv"] },
-  bulkDiscount: { rule: () => {}, products: ["ipd"] },
-  adapterBundle: { rule: () => {}, products: ["mbp"] },
-}
+  threeForTwo: {
+    rule: (products: Product[]): number => {
+      const productCount = products.length;
+      const unitPrice = products[0].price;
+      return Math.floor(productCount / 3) * unitPrice;
+    },
+    products: ["atv"],
+  },
+  bulkDiscount: { rule: (products: Product[]): number => {
+    const productCount = products.length;
+    const unitPrice = products[0].price;
+    const discount = unitPrice - (unitPrice * 0.9090892561682939)
+    return productCount > 4 ? productCount * discount : 0;
+  }, products: ["ipd"] },
+  adapterBundle: { rule: (products: Product[]): number => { 
+    const productCount = products.length;
+    return productCount * 30.0;
+  }, products: ["mbp"] },
+};
 
 export class Checkout {
   pricingRules: object;
-  scannedSKUs: string[];
+  scannedSKUs: Product[];
 
   constructor(pricingRules: object) {
     this.pricingRules = pricingRules;
@@ -33,30 +47,19 @@ export class Checkout {
   }
 
   scan(product: Product) {
-    this.scannedSKUs.push(product.sku);
+    this.scannedSKUs.push(product);
   }
 
   total(): string {
-    const discounts = Object.entries(this.pricingRules).map(([name, pricingRule]) => {
-      const { rule, products } = pricingRule;
-      const applicableProducts = this.scannedSKUs.filter( sku => products.includes(sku) )
-      return rule(applicableProducts);
-    });
-    // console.log("discounts", discounts)
+    const discounts = Object.entries(this.pricingRules).map(
+      ([name, pricingRule]) => {
+        const { rule, products } = pricingRule;
+        const applicableProducts = this.scannedSKUs.filter((sku) =>
+          products.includes(sku.name)
+        );
+        return rule(applicableProducts);
+      }
+    );
     return "hello world";
   }
 }
-
-const checkout = new Checkout(pricingRules);
-checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 })
-checkout.scan({ sku: "ipd", name: "Super iPad", price: 549.99 })
-checkout.scan({ sku: "mbp", name: "MacBook Pro", price: 1399.99 })
-checkout.scan({ sku: "mbp", name: "MacBook Pro", price: 1399.99 })
-
-checkout.scan({ sku: "atv", name: "Apple TV", price: 109.5 })
-checkout.scan({ sku: "atv", name: "Apple TV", price: 109.5 })
-
-checkout.scan({ sku: "vga", name: "VGA adapter", price: 30.0 })
-checkout.scan({ sku: "vga", name: "VGA adapter", price: 30.0 })
-
-console.log("total", checkout.total());
